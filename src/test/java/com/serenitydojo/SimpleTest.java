6c02aa9cc955904b27fd347d80cd12e6e28c93d4
John Ferguson Smart <john.smart@wakaleo.com>
2022-08-31 13:11:51 +0100

    First exercise.

---
+++ src/test/java/com/serenitydojo/SimpleTest.java
@@ -0,0 +1,10 @@
+package com.serenitydojo;
+
+import org.junit.Test;
+
+public class SimpleTest {
+    @Test
+    public void shouldPrintHelloWorld() {
+        System.out.println("Hello world");
+    }
+}

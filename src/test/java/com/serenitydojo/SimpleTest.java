d0a97618479809eb753448637468b14446d670bf
John Ferguson Smart <john.smart@wakaleo.com>
2022-08-31 13:14:20 +0100

    First exercise.

--- src/test/java/com/serenitydojo/SimpleTest.java
+++ src/test/java/com/serenitydojo/SimpleTest.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 public class SimpleTest {
     @Test
     public void shouldPrintHelloWorld() {
+        // Wasn't sure about this, should the message be a variable
         System.out.println("Hello world");
     }
 }
